97b5b5a0dfdefadbbf54c0e8523d796cb6d11661
Netanel and Gabi <gabigutkin@gmail.com>
2021-12-12 00:23:35 +0200

    hide and show password, useState for inputs

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button,ImageBackground } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button,ImageBackground,TouchableOpacity } from 'react-native';
 import { Entypo  } from '@expo/vector-icons';
 
+
+
 export default function HomeScreen() {
+  const [userName, setUserName]=useState('');
+  const [userPassword, setUserPassword]=useState('');
+  const [secure, setSecure]=useState(true);
+  const [EntypoValue, setEntypo]=useState("eye-with-line")
+
+  const eyePresses=()=>{
+    if (secure==true){
+      setSecure(false)
+    } 
+  else{
+    setSecure(true)
+  }
+  if (EntypoValue=="eye-with-line"){
+    setEntypo("eye")
+  }
+  else{
+    setEntypo("eye-with-line")
+  }
+  }
+
   return (
     <ImageBackground source={require('../assets/backgroundForApp.png')} style={styles.container}>
         <Text style={styles.text}>שם משתמש:</Text>
         
-        <TextInput style={styles.inputUser} placeholder='הכנס שם משתמש:'></TextInput>
+        <TextInput placeholderTextColor={'#394150'} style={styles.inputUser} placeholder='הכנס שם משתמש:'
+          onChangeText={(valUser)=>setUserName(valUser)} ></TextInput>
         <Text style={styles.text}>סיסמא:</Text>
-        <View style={styles.logoEye} style={styles.inputPass} >
-        <Entypo  name="eye-with-line" size={17} color="black" />
-        <TextInput placeholder='הכנס סיסמא:' secureTextEntry={true}></TextInput>
+        <View  style={styles.inputPass} >
+          <TouchableOpacity>
+            <Entypo  name={EntypoValue} size={20} color="black" onPress={()=>eyePresses()} />
+            </TouchableOpacity>
+          <TextInput placeholderTextColor={'#394150'} placeholder='הכנס סיסמא:' secureTextEntry={secure}
+            onChangeText={(valPassword)=>setUserPassword(valPassword)}></TextInput>
         </View>
-        <Button title='כניסה'></Button>
+        <Button title='כניסה' onPress={()=>console.log("username: ", userName, "Password: ", userPassword)}></Button>
     </ImageBackground>
    
   );
@@ -34,24 +60,23 @@ const styles = StyleSheet.create({
     borderWidth:3,
     padding: 8,
     margin: 10,
-    width: 250,
+    width: 200,
     textAlign:'right',
     alignContent:'center',
     borderRadius:20,
-    padding:10,
   },
   inputPass:{
     borderWidth:3,
     padding: 8,
     margin: 10,
-    width: 250,
+    width: 200,
     textAlign:'right',
     alignContent:'center',
     borderRadius:20,
-    padding:10,
     flexDirection:'row',
   },
   logoEye:{
     flexDirection:'row',
+    padding: 10,
   }
 });
