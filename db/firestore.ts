0b8640f23729ea6ecb46b8253d748d2fff2fd58d
netanel <netanelha@post.jce.ac.il>
2021-12-29 20:23:53 +0200

    firebase install
    firebase install

---
+++ db/firestore.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-7RT0TdnmSFh4SPOAgP44jkmS2szUBeM",
+  authDomain: "dementiaapp-8b292.firebaseapp.com",
+  projectId: "dementiaapp-8b292",
+  storageBucket: "dementiaapp-8b292.appspot.com",
+  messagingSenderId: "436569858334",
+  appId: "1:436569858334:web:7315a5e32d6a8ff63d290f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
